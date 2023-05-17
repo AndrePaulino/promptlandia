865db2f135344c8a7ac7a79fd67a7ca83273573d
Andre Paulino <andrepaulinobtw@gmail.com>
2023-05-17 04:36:02 -0300

    🗞️ Create Provider component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
-import Nav from "@components/Nav";
 import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
 	title: "Promptopolis",
@@ -11,14 +12,16 @@ const RootLayout = ({ children }) => {
 	return (
 		<html lang="pt-br">
 			<body>
-				<div className="main">
-					<div className="gradient" />
-				</div>
+				<Provider>
+					<div className="main">
+						<div className="gradient" />
+					</div>
 
-				<main className="app">
-					<Nav />
-					{children}
-				</main>
+					<main className="app">
+						<Nav />
+						{children}
+					</main>
+				</Provider>
 			</body>
 		</html>
 	);
