8f971be58cd8fd49530e36128f347b6e273e5f50
Andre Paulino <andrepaulinobtw@gmail.com>
2023-05-18 04:09:41 -0300

    🗞️ Created Feed component

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
 	return (
 		<section className="w-full flex-center flex-col">
@@ -12,7 +14,7 @@ const Home = () => {
 				e compartilhar prompts criativos para IAs.
 			</p>
 
-			{/* Feed */}
+			<Feed />
 		</section>
 	);
 };
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,5 +1,54 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+	return (
+		<div className="mt-16 prompt_layout">
+			{data.map((post) => (
+				<PromptCard
+					key={post._id}
+					post={post}
+					handleTagClick={handleTagClick}
+				/>
+			))}
+		</div>
+	);
+};
+
 const Feed = () => {
-	return <div>Feed</div>;
+	const [searchText, setSearchText] = useState("");
+	const [posts, setPosts] = useState([]);
+	const handleSearchChange = async (event) => {};
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch("/api/prompt");
+			const data = await response.json();
+
+			setPosts(data);
+		};
+
+		fetchPosts();
+	}, []);
+
+	return (
+		<section className="feed">
+			<form className="relative w-full flex-center">
+				<input
+					className="search_input peer"
+					onChange={handleSearchChange}
+					placeholder="Procure por uma tag ou usuário"
+					required
+					type="text"
+					value={searchText}
+				/>
+			</form>
+
+			<PromptCardList data={posts} handleTagClick={() => {}} />
+		</section>
+	);
 };
 
 export default Feed;
