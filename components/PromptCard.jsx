e27608a0e69489ddf5a4154ba6bc945fc41785d1
Andre Paulino <andrepaulinobtw@gmail.com>
2023-05-22 00:51:43 -0300

    🗞️ Added edit & delete to PromptCard component

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,6 +6,9 @@ import { usePathname, useRouter } from "next/navigation";
 import Image from "next/image";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+	const { data: session } = useSession();
+	const pathName = usePathname();
+	const router = useRouter();
 	const [copied, setCopied] = useState("");
 
 	const handleCopy = () => {
@@ -56,8 +59,26 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 				className="font-inter text-sm blue_gradient cursor-pointer"
 				onClick={() => handleTagClick && handleTagClick(post.tag)}
 			>
-				{post.tag}
+				#{post.tag}
 			</p>
+
+			{session?.user.id === post.creator._id &&
+				pathName === "/profile" && (
+					<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+						<p
+							className="font-inter text-sm green_gradient cursor-pointer"
+							onClick={handleEdit}
+						>
+							Editar
+						</p>
+						<p
+							className="font-inter text-sm orange_gradient cursor-pointer"
+							onClick={handleDelete}
+						>
+							Deletar
+						</p>
+					</div>
+				)}
 		</div>
 	);
 };
